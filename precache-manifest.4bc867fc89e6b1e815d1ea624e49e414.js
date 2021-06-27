053d703b76d8399582e841ecac0c2a950c7dad64
Mohammad Ghiasi <emg110@gmail.com>
2021-06-28 00:40:17 +0430

    - added new version

---
+++ precache-manifest.4bc867fc89e6b1e815d1ea624e49e414.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c043ef98b4c1929e2ebd1bbb909d11",
+    "url": "/algorandqrcodereact/index.html"
+  },
+  {
+    "revision": "48bfbcd21885d9fffc9b",
+    "url": "/algorandqrcodereact/static/css/2.fe7d74ce.chunk.css"
+  },
+  {
+    "revision": "792c883183da848393a4",
+    "url": "/algorandqrcodereact/static/css/main.f156885b.chunk.css"
+  },
+  {
+    "revision": "48bfbcd21885d9fffc9b",
+    "url": "/algorandqrcodereact/static/js/2.77c3043b.chunk.js"
+  },
+  {
+    "revision": "40afde78132124db8c46b4b0e2f610df",
+    "url": "/algorandqrcodereact/static/js/2.77c3043b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792c883183da848393a4",
+    "url": "/algorandqrcodereact/static/js/main.33cc07ff.chunk.js"
+  },
+  {
+    "revision": "2d902208099326afdacf",
+    "url": "/algorandqrcodereact/static/js/runtime-main.b93f7f64.js"
+  },
+  {
+    "revision": "7c807e9ee4c69b2611c6978fc1b7a294",
+    "url": "/algorandqrcodereact/static/media/Rand-Labs-color.7c807e9e.png"
+  },
+  {
+    "revision": "5d72f5eef9d36865fcedc1296c926005",
+    "url": "/algorandqrcodereact/static/media/algorand-qrcode-banner.5d72f5ee.jpg"
+  },
+  {
+    "revision": "50bbe1d180d9cdfde6a8c078c8a90c99",
+    "url": "/algorandqrcodereact/static/media/algorand_full_logo_black.50bbe1d1.svg"
+  },
+  {
+    "revision": "6079b8077579acf4a3b2c2765293476c",
+    "url": "/algorandqrcodereact/static/media/myalgo-blue.6079b807.6079b807.png"
+  },
+  {
+    "revision": "633775c859b6b5a4d030ef2cc460afcd",
+    "url": "/algorandqrcodereact/static/media/qrlogo.633775c8.svg"
+  },
+  {
+    "revision": "a35276e5768605883d5fbe372d2c902b",
+    "url": "/algorandqrcodereact/static/media/sidebar-1.a35276e5.jpg"
+  },
+  {
+    "revision": "136425fb776787fae1188e0079dc47da",
+    "url": "/algorandqrcodereact/static/media/sidebar-2.136425fb.jpg"
+  },
+  {
+    "revision": "1f2624b42b6b3d333ee1b45b92a5ceaf",
+    "url": "/algorandqrcodereact/static/media/sidebar-3.1f2624b4.jpg"
+  },
+  {
+    "revision": "5d72f5eef9d36865fcedc1296c926005",
+    "url": "/algorandqrcodereact/static/media/sidebar-4.5d72f5ee.jpg"
+  }
+]);
